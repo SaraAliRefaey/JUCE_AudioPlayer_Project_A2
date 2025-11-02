b709f5bf3524e0380c8dceac821561bbaf39e5bc
haneen-fouad2 <20240175@stud.fci-cu.edu.eg>
2025-11-02 23:13:01 +0200

    Update MainComponent.cpp

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -1,12 +1,19 @@
 ﻿#include "MainComponent.h"
 
 MainComponent::MainComponent()
+    : playerGUI1(player1),
+    playerGUI2(player2),
+    playlistComponent(player1)
 {
 
-    addAndMakeVisible(player1);
-
-    setSize(500, 250);
     setAudioChannels(0, 2);
+
+    addAndMakeVisible(playerGUI1);
+    addAndMakeVisible(playerGUI2);
+  
+    addAndMakeVisible(playlistComponent);
+
+    setSize(800, 600);
 }
 
 MainComponent::~MainComponent()
@@ -17,19 +24,48 @@ MainComponent::~MainComponent()
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
     player1.prepareToPlay(samplesPerBlockExpected, sampleRate);
+    player2.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
-{
-    player1.getNextAudioBlock(bufferToFill);
+{ bufferToFill.clearActiveBufferRegion();
+
+ juce::AudioBuffer<float> temp(bufferToFill.buffer->getNumChannels(), bufferToFill.numSamples);
+ juce::AudioSourceChannelInfo tempInfo(&temp, 0, bufferToFill.numSamples);
+
+      
+ player1.getNextAudioBlock(tempInfo);
+ for (int ch = 0; ch < bufferToFill.buffer->getNumChannels(); ++ch)
+     bufferToFill.buffer->addFrom(ch, bufferToFill.startSample, temp, ch, 0, bufferToFill.numSamples);
+
+ 
+ temp.clear();
+ player2.getNextAudioBlock(tempInfo);
+ for (int ch = 0; ch < bufferToFill.buffer->getNumChannels(); ++ch)
+     bufferToFill.buffer->addFrom(ch, bufferToFill.startSample, temp, ch, 0, bufferToFill.numSamples);
 }
 
 void MainComponent::releaseResources()
 {
     player1.releaseResources();
+     player2.releaseResources();
+
+}
+
+
+void MainComponent::paint(juce::Graphics& g)
+{
+    g.fillAll(juce::Colours::darkgrey);
 }
 
 void MainComponent::resized()
 {
-    player1.setBounds(getLocalBounds());
+    auto area = getLocalBounds();
+
+    auto top = area.removeFromTop(getHeight() / 2);
+    playerGUI1.setBounds(top.removeFromLeft(getWidth() / 2));
+    playerGUI2.setBounds(top);
+
+   
+    playlistComponent.setBounds(area);
 }
